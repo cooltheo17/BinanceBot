8d8f51c4b8296bd7e357242b933a3a90c4fc7ceb
Theo <theo17a@gmail.com>
2021-03-26 21:28:03 +0200

    Update botStarter.py

--- botStarter.py
+++ botStarter.py
@@ -17,18 +17,19 @@ def priceUpdater():
     for candlestick in candles:
         candlestick_writer.writerow(candlestick) 
 
-client = Client(config.API_KEY, config.API_SECRET)
-priceUpdater()
-my_data = genfromtxt('BTC_LATEST.csv', delimiter=',')
-close_btc = my_data[:,4]
-i=len(my_data)-1
-
-#Calculate macd and macd signal values
-allIndexes=talib.MACD(close_btc, fastperiod=12, slowperiod=26, signalperiod=9)
-macd=allIndexes[2]
 print("Staring Bot")
 print("================")
 while(1):
+    client = Client(config.API_KEY, config.API_SECRET)
+    priceUpdater()
+    my_data = genfromtxt('BTC_LATEST.csv', delimiter=',')
+    close_btc = my_data[:,4]
+    i=len(my_data)-1
+
+    #Calculate macd and macd signal values
+    allIndexes=talib.MACD(close_btc, fastperiod=12, slowperiod=26, signalperiod=9)
+    macd=allIndexes[2]
+    
     #We are at selling point
     if macd[i] < 0 and macd[i-1] > 0 and float(macd[i]) < -1:
         #Cancel any BTCUP orders
@@ -37,8 +38,8 @@ while(1):
             orderId=OpenOrder[0].get('orderId')
             client.cancel_order(symbol='BTCUPUSDT',orderId=orderId)
             time.sleep(10)  
-        #Sell any BTCUP
-        balance_BTCUP = round(float(client.get_asset_balance(asset='BTCUP').get('free')),2)+round(float(client.get_asset_balance(asset='BTCUP').get('locked')),2)
+        #Sell any BTCUP    
+        balance_BTCUP = round(float(client.get_asset_balance(asset='BTCUP').get('free')),2)+round(float(client.get_asset_balance(asset='BTCUP').get('locked')),2)-0.01
         if (balance_BTCUP > 0.1):
             client.order_market_sell(symbol='BTCUPUSDT',quantity=balance_BTCUP)
             time.sleep(10)    
@@ -71,7 +72,8 @@ while(1):
         if(balance_BTCUP < 0.12):
             print("Buying Point")
             buyingPrice = round(float(client.get_avg_price(symbol='BTCUPUSDT').get('price')),3)
-            balance = round(float(client.get_asset_balance(asset='USDT').get('free')),2)-0.5
+            balance = round(float(client.get_asset_balance(asset='USDT').get('free')),2)-1
+            print(balance)
             quantity=round(balance/buyingPrice,2)
             client.order_market_buy(symbol='BTCUPUSDT',quantity=quantity)
             time.sleep(5)

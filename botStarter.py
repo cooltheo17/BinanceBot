21c68543bc67948395ec787eac3ce52dab72468a
Theo <theo17a@gmail.com>
2021-03-28 23:19:15 +0300

    Colour changes and imrpovments

--- botStarter.py
+++ botStarter.py
@@ -24,37 +24,41 @@ def priceUpdater():
     for candlestick in candles:
         candlestick_writer.writerow(candlestick) 
 
-print("Staring Bot")
-print("================")
+def marketEnter(enterNow): 
+  
+    while(enterNow==0):
+        
+        #Get all price data
+        priceUpdater()
+        my_data = genfromtxt('BTC_LATEST.csv', delimiter=',')
+        close_btc = my_data[:,4]
+        i=len(my_data)-1
+        
+        #Calculate macd values
+        allIndexes=talib.MACD(close_btc, fastperiod=12, slowperiod=26, signalperiod=9)
+        macd=allIndexes[2]
+        
+        #Check if MACD is performing an interection
+        if (float(macd[i]) < -10 and float(macd[i-1]) > 0) or (float(macd[i]) > 10 and float(macd[i-1]) < 0):
+            print('\033[33;94m- Entering market -\033[0m',)
+            print(datetime.now().strftime('%H:%M:%S %d-%m '))
+            enterNow=1
+        else:
+            print("- Waiting -")
+            print(datetime.now().strftime('%H:%M:%S %d-%m '))
+        time.sleep(117)      
+print('\033[33;93m- Staring Bot -',)
+print("================\033[0m")
 
-#If you wihs to enter market now change this to 0 otherwise it will automatically enter the market at an intersection
-enterNow=1
-while(enterNow==0):
-    #Connect the Binance API
-    client = Client(config.API_KEY, config.API_SECRET)
-    
-    #Get all price data
-    priceUpdater()
-    my_data = genfromtxt('BTC_LATEST.csv', delimiter=',')
-    close_btc = my_data[:,4]
-    i=len(my_data)-1
-    
-    #Calculate macd values
-    allIndexes=talib.MACD(close_btc, fastperiod=12, slowperiod=26, signalperiod=9)
-    macd=allIndexes[2]
-    
-    #Check if MACD is performing an interection
-    if (float(macd[i]) < -10 and float(macd[i-1]) > 0) or (float(macd[i]) > 10 and float(macd[i-1]) < 0):
-        print("- Entering market -")
-        print(datetime.now().strftime('%H:%M:%S %d-%m '))
-        enterNow=1
-    else:
-        print("- Waiting -")
-        print(datetime.now().strftime('%H:%M:%S %d-%m '))
-    time.sleep(17)      
+#If you wish to enter market now change this to 1 otherwise it will automatically enter the market at an intersection
+marketEnterIndex=0
 
 #Maintain bot after we get in the market
 while(True):
+
+    #Check if it is time to join the market
+    marketEnter(marketEnterIndex)
+    
     #Connect the Binance API
     client = Client(config.API_KEY, config.API_SECRET)
     
@@ -84,19 +88,19 @@ while(True):
         #Buy BTCDOWN
         balance_BTCDOWN = round(float(client.get_asset_balance(asset='BTCDOWN').get('free')),2)+round(float(client.get_asset_balance(asset='BTCDOWN').get('locked')),2)
         if(balance_BTCDOWN < 300):
-            print("Selling Point")
-            print(datetime.now().strftime('%H:%M:%S %d-%m '))
+            print('\033[33;91m- Selling Point -\033[0m',)
+            print(datetime.now().strftime(' %H:%M:%S %d-%m '))
             buyingPrice = round(float(client.get_avg_price(symbol='BTCDOWNUSDT').get('price')),4)
             balance = round(float(client.get_asset_balance(asset='USDT').get('free')),2)-0.5
             quantity=round(balance/buyingPrice,2)
             client.order_market_buy(symbol='BTCDOWNUSDT',quantity=quantity)
             time.sleep(5)
-            stopLossPrice = round(buyingPrice*0.97,4)
-            takeProfitPrice = round(buyingPrice*1.08,4)
+            stopLossPrice = round(buyingPrice*0.984,4)
+            takeProfitPrice = round(buyingPrice*1.07,4)
             client.order_oco_sell(symbol='BTCDOWNUSDT', quantity=quantity, price=takeProfitPrice, stopPrice=stopLossPrice, stopLimitPrice=stopLossPrice, stopLimitTimeInForce='GTC')
         else:
             print("- Waiting -")
-            print(datetime.now().strftime('%H:%M:%S %d-%m '))      
+            print(datetime.now().strftime(' %H:%M:%S %d-%m '))      
     elif float(macd[i]) > 10: 
         #Cancel any BTCDOWN orders
         OpenOrder = client.get_open_orders(symbol='BTCDOWNUSDT')
@@ -112,19 +116,19 @@ while(True):
         #Buy BTCUP
         balance_BTCUP = round(float(client.get_asset_balance(asset='BTCUP').get('free')),2)+round(float(client.get_asset_balance(asset='BTCUP').get('locked')),2)
         if(balance_BTCUP < 0.12):
-            print("Buying Point")
-            print(datetime.now().strftime('%H:%M:%S %d-%m '))
+            print('\033[33;92m- Buying Point -\033[0m',)
+            print(datetime.now().strftime(' %H:%M:%S %d-%m '))
             buyingPrice = round(float(client.get_avg_price(symbol='BTCUPUSDT').get('price')),3)
             balance = round(float(client.get_asset_balance(asset='USDT').get('free')),2)-0.5
             quantity=round(balance/buyingPrice,2)
             client.order_market_buy(symbol='BTCUPUSDT',quantity=quantity)
             time.sleep(5)
-            stopLossPrice = round(buyingPrice*0.97,3)
-            takeProfitPrice = round(buyingPrice*1.08,3)
+            stopLossPrice = round(buyingPrice*0.984,3)
+            takeProfitPrice = round(buyingPrice*1.07,3)
             client.order_oco_sell(symbol='BTCUPUSDT', quantity=quantity, price=takeProfitPrice, stopPrice=stopLossPrice, stopLimitPrice=stopLossPrice, stopLimitTimeInForce='GTC')
         else:
             print("- Waiting -")
-            print(datetime.now().strftime('%H:%M:%S %d-%m '))    
-    time.sleep(177)             
+            print(datetime.now().strftime(' %H:%M:%S %d-%m '))    
+    time.sleep(117)             
 
 
